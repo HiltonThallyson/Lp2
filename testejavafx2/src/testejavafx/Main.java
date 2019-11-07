322712da89b9778a4b8a24fb0f67410f7c845cc9
HiltonThallyson <hilton.thallyson@hotmail.com>
2019-11-07 19:07:29 -0300

    Mudanças na janela de exibição

--- testejavafx2/src/testejavafx/Main.java
+++ testejavafx2/src/testejavafx/Main.java
@@ -1,9 +1,6 @@
 package testejavafx;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.Event;
-import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -13,9 +10,10 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-public class Main extends Application implements EventHandler<ActionEvent> {
+public class Main extends Application {
 
     Button button1;
     Button button2;
@@ -28,6 +26,9 @@ public class Main extends Application implements EventHandler<ActionEvent> {
     Image foto2 = new Image("https://i.ytimg.com/vi/BBLBju39ewM/maxresdefault.jpg");
     Image foto3 = new Image("https://www.opovo.com.br/esportesimages/app/noticia_14970375377/2018/07/02/29193/hqdefault.jpg");
     ImageView image = new ImageView(foto0);
+
+    int screenWidth = 900;
+    int screenHeight = 800;
     public static void main(String[] args) {
 	    launch(args);
     }
@@ -35,50 +36,39 @@ public class Main extends Application implements EventHandler<ActionEvent> {
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Title of the windows");
-        image.setFitHeight(450);
-        image.setFitWidth(250);
+        image.setFitHeight(screenHeight);
+        image.setFitWidth(screenWidth/2);
+        image.setSmooth(true);
         label = new Label();
         label.setText("Kiko");
+        label.setTextFill(Color.WHITE);
         button1  = new Button();
         button1.setText("Hit");
-       // button1.setOnAction(e -> label.setText("Ahhh!!Mamãeeee"), telaHorizontal.getChildren().addAll(foto1));
-        button1.setOnAction(this);
+        button1.setOnAction(e -> {label.setText("Aaah! Mamãeeeee"); label.setTextFill(Color.WHITE); image.setImage(foto1);});
+
         button2 = new Button();
         button2.setText("Ugly");
-       // button2.setOnAction(e -> label.setText("Você não vai com a minha cara?"));
-        button2.setOnAction(this);
+        button2.setOnAction(e -> {label.setText("Você não vai com a minha cara?"); label.setTextFill(Color.WHITE); image.setImage(foto2);});
 
         button3 = new Button();
         button3.setText("Cry");
-       // button3.setOnAction(e -> label.setText("Bruuuuu"));
-        button3.setOnAction(this);
+        button3.setOnAction(e -> {label.setText("Bruuuuuu"); label.setTextFill(Color.WHITE);image.setImage(foto3);});
 
-        menuVertical = new VBox(8);
+        menuVertical = new VBox(20);
         menuVertical.getChildren().addAll(label, button1, button2, button3);
         menuVertical.setAlignment(Pos.CENTER);
+        menuVertical.setPrefWidth(screenWidth/2);
 
         telaHorizontal = new HBox();
         telaHorizontal.getChildren().addAll(menuVertical, image);
         telaHorizontal.setAlignment(Pos.BOTTOM_LEFT);
         StackPane layout = new StackPane();
         layout.getChildren().add(telaHorizontal);
+        layout.setStyle("-fx-background-color: black");
 
-        Scene scene1 = new Scene(layout, 500, 450);
+        Scene scene1 = new Scene(layout, screenWidth, screenHeight);
         primaryStage.setScene(scene1);
         primaryStage.show();
     }
 
-    @Override
-    public void handle(ActionEvent event) {
-        if(event.getSource().equals(button1)){
-            label.setText("Aaah! Mamãeeeeee");
-            image.setImage(foto1);
-        }else if(event.getSource().equals(button2)){
-            label.setText("Você não vai com a minha cara?");
-            image.setImage(foto2);
-        }else{
-            label.setText("Bruuuuuaaaa");
-            image.setImage(foto3);
-        }
-    }
 }
